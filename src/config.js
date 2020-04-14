d02eb4c76cbf726e7e9a05a0038e0c68d0f3757e
Ron Martin <ronmartin89@gmail.com>
2020-04-13 17:16:30 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: 'https://ancient-spire-88409.herokuapp.com/api' || process.env.REACT_APP_API_BASE_URL ||'http://localhost:8000',
+  API_ENDPOINT: 'https://ancient-spire-88409.herokuapp.com/' || process.env.REACT_APP_API_BASE_URL ||'http://localhost:8000',
 };
